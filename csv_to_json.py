19eb118df84abe824a674faa569819e33179d6ae
AndyArch11 <59818257+AndyArch11@users.noreply.github.com>
2020-04-27 18:41:59 +1000

    Fixed missing prison coords, added csv_to_json

---
+++ csv_to_json.py
@@ -0,0 +1,15 @@
+import csv
+import json
+
+csv_prison_file = './prison_list.csv'
+json_prison_file = './prison_list.json'
+
+data = []
+with open (csv_prison_file) as csvFile:
+    csvReader = csv.DictReader(csvFile)
+    for csvRow in csvReader:
+        if 'Operational' in csvRow['Status']:
+            data.append(csvRow)
+
+with open(json_prison_file, "w") as jsonFile:
+    jsonFile.write(json.dumps(data, indent = 4))
